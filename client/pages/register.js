8bee36cc7bc1db8b2977f2733f29970588725244
JangGeunSong <sjg1468@gmail.com>
2019-07-31 21:29:29 +0900

    register form add

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
 import Link from 'next/link'
+import { Mutation, ApolloConsumer } from 'react-apollo'
+import gql from 'graphql-tag'
 
 import './pageStyle.css'
 
@@ -7,11 +9,32 @@ import Navbar from '../components/Navbar/Navbar';
 
 class register extends Component {
 
+    state = {
+        isRegisterComplete: false
+    }
+
     componentDidMount() {
         document.title = "Register"
     }
 
+    // name: String!
+    // email: String!
+    // password: String!
+    // profile_image: String
+    // profile: String
+
     render() {
+
+        const CREATE_USER = gql `
+            mutation createUser($name: String!, $email: String!, $password: String!, $profile_image: String, $profile: String) {
+                createUser(userInput: { name: $name, email: $email, password: $password, profile_image: $profile_image, profile: $profile }) {
+                    _id
+                }
+            }
+        `
+
+        let NAME, EMAIL, PASSWORD, PROFILE_IMG, PROFILE;
+
         return (
             <div>
                 <div className="title">
@@ -31,7 +54,36 @@ class register extends Component {
                     </div>
                 <Navbar />
                 <div className="contentContainer">
-                    Welcome to Register page
+                    <ApolloConsumer>
+                        {client => {
+                            <Mutation
+                                mutation={CREATE_USER}
+                                onCompleted={() => {
+                                    this.setState({ isRegisterComplete: true });
+                                }}
+                            >
+                                {(createuser, { loading, error }) => {
+                                    if(loading) return <p>Loading...</p>
+                                    if(error) return <p>Error is occured!</p>
+
+                                    return (
+                                        <form className="form__control" onSubmit={e => {
+                                            e.preventDefault()
+                                            createuser({ variable: { name: NAME.value, email: EMAIL.value, password: PASSWORD.value, profile_image: PROFILE_IMG.value, profile: PROFILE.value } })
+                                        }}>
+                                            <h1>Register Page</h1>
+                                            <input type="text" placeholder="email" ref={emailValue => { EMAIL = emailValue }}/>
+                                            <input type="password" placeholder="password" ref={passwordValue => { PASSWORD = passwordValue }}/>
+                                            <input type="text" placeholder="name" ref={nameValue => { NAME = nameValue }}/>
+                                            <input type="text" placeholder="profile" ref={profileValue => { PROFILE = profileValue }}/>
+                                            <input type="text" placeholder="profile_image" ref={profileIMGValue => { PROFILE_IMG = profileIMGValue }}/>
+                                            <button className="form__button">Submit</button>
+                                        </form>
+                                    )
+                                }}
+                            </Mutation>
+                        }}
+                    </ApolloConsumer>
                 </div>
             </div>
         )
