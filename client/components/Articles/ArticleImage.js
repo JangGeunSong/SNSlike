37b5408b85d6f3e4600c8ecee3110b3c7c363e54
Janggeun Song <sjg1468@gmail.com>
2019-08-25 00:21:04 +0900

    Image change work complete!

---
+++ client/components/Articles/ArticleImage.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react'
+
+import './Articles.css'
+
+export class ArticleImage extends Component {
+    state = {
+        images: this.props.images,
+        tarNum: 0,
+    }
+    
+    showImage = {
+        display: 'block'
+    }
+
+    noShowImage = {
+        display: 'none'
+    }
+
+    componentDidMount() {
+        this.imageLists = this.state.images.map((image, number) => {
+            if(number === this.state.tarNum) {
+                return (
+                    <img className="Article__image" key={number} style={ this.showImage } src={`http://localhost:5500/static/article/${image}`} alt={`${image}`}/>
+                )
+            }
+            else {
+                return (
+                    <img className="Article__image" key={number} style={ this.noShowImage } src={`http://localhost:5500/static/article/${image}`} alt={`${image}`}/>
+                )
+            }
+        });
+    }
+
+    render() {
+        console.log(this.state.tarNum)
+        return (
+            <div>
+                {
+                    this.state.images.map((image, number) => {
+                        if(number === this.state.tarNum) {
+                            return (
+                                <img className="Article__image" key={number} style={ this.showImage } src={`http://localhost:5500/static/article/${image}`} alt={`${image}`}/>
+                            )
+                        }
+                        else {
+                            return (
+                                <img className="Article__image" key={number} style={ this.noShowImage } src={`http://localhost:5500/static/article/${image}`} alt={`${image}`}/>
+                            )
+                        }
+                    })
+                }
+                <button className="Article__button" onClick={e => {
+                    e.preventDefault();
+                    let nextNum = this.state.tarNum + 1;
+                    this.setState({ tarNum: nextNum });
+                    if(nextNum >= this.state.images.length) {
+                        this.setState({ tarNum: 0 });
+                    }
+                    this.forceUpdate()
+                }}>Next</button>
+            </div>
+        )
+    }
+}
+
+export default ArticleImage
--- client/components/Articles/Articles.js
+++ client/components/Articles/Articles.js
@@ -1,12 +1,15 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Query, Mutation } from 'react-apollo'
 import gql from 'graphql-tag'
 import Moment from 'react-moment'
 
+import ArticleImage from './ArticleImage';
+
 import './Articles.css'
 
-function Articles() {
-    const GET_ARTICLES = gql`
+class Articles extends Component {
+
+    GET_ARTICLES = gql`
         query {
             articles {
                 _id
@@ -22,7 +25,7 @@ function Articles() {
         }
     `;
 
-    const DELETE_ARTICLE = gql`
+    DELETE_ARTICLE = gql`
         mutation deleteArticle ($articleId: ID!) {
             deleteArticle(articleId: $articleId) {
                 _id
@@ -35,123 +38,69 @@ function Articles() {
                 }
             }
         }
-    ` 
+    ` ;
 
-    const articleImageShowing = (articleImages) => {
-        return articleImages
+    componentDidMount() {
+
     }
 
-    return (
-        <Query query={GET_ARTICLES}>
-            {({ loading, error, data }) => {
-                if (loading) return 'Loading...';
-                if (error) return `Error! ${error.message}`;
+    render() {
+        return (
+            <Query query={this.GET_ARTICLES}>
+                {({ loading, error, data }) => {
+                    if (loading) return 'Loading...';
+                    if (error) return `Error! ${error.message}`;
 
-                let resultArticle = data.articles;
+                    let resultArticle = data.articles;
 
-                return (
-                    <div className="Articles">
-                        {resultArticle.map(article => {
-                            let isImageEmpty = true;
-                            let articleImage = <p></p>;
-                            let recentIndex = 0;
-                            let imageNumber = 0;
-                            const showStyle = {display: 'block'}  
-                            const noShowStyle = {display: 'none'}  
-                            if(article.images === null) {
-                                isImageEmpty = true;
-                            }
-                            else {
-                                isImageEmpty = false;
-                                imageNumber = article.images.length;
-                                articleImage = article.images.map((image, number)=> {
-                                    if(number === 0) {
-                                        return (
-                                            <img className="Article__image" key={number} style={showStyle} src={`http://localhost:5500/static/article/${image}`} alt={`${image}`}/>
-                                        )
-                                    }
-                                    else {
-                                        return (
-                                            <img className="Article__image" key={number} style={noShowStyle} src={`http://localhost:5500/static/article/${image}`} alt={`${image}`}/>
-                                        )
-                                    }
-                                })
-                            }
-                            return (
-                                <Mutation 
-                                    key={article._id} 
-                                    mutation={DELETE_ARTICLE}
-                                    refetchQueries={() => {
-                                        console.log("Refetch Query run");
-                                        return [
-                                            {
-                                                query: GET_ARTICLES,
-                                                variables: resultArticle
-                                            }
-                                        ]
-                                    }}
-                                >
-                                    {deleteArticle => (
-                                        <div className="Article">
-                                            <div className="Article__contents">
-                                                <h1>{article.title}</h1>
-                                                {isImageEmpty ? 
-                                                (<p></p>)
-                                                :
-                                                (
-                                                    <div className="Article__imageCage">
-                                                        {articleImage}
-                                                    </div>
-                                                )
+                    return (
+                        <div className="Articles">
+                            {resultArticle.map(article => {
+                                const imageNum = article.images.length;
+                                let tarnum = 0;
+                                return (
+                                    <Mutation 
+                                        key={article._id} 
+                                        mutation={this.DELETE_ARTICLE}
+                                        refetchQueries={() => {
+                                            console.log("Refetch Query run");
+                                            return [
+                                                {
+                                                    query: this.GET_ARTICLES,
+                                                    variables: resultArticle
                                                 }
+                                            ]
+                                        }}
+                                    >
+                                        {deleteArticle => (
+                                            <div className="Article">
+                                                <div className="Article__contents">
+                                                    <h1>{article.title}</h1>
+                                                    <ArticleImage images={article.images}/>
+                                                    <h2>{article.description}</h2>
+                                                    <Moment format="LLLL" local>{article.date}</Moment>
+                                                </div>
+                                                <div className="User__container">
+                                                    <img className="User__profile" src={`http://localhost:5500/static/images/${article.writer.profile_image}`} alt={`${article.writer.name}'s image`}/>
+                                                    <p className="User__name">{article.writer.name}</p>
+                                                </div>
                                                 <button className="Article__button" onClick={e => {
                                                     e.preventDefault();
-                                                    const prevNum = recentIndex;
-                                                    recentIndex++;
-                                                    if(recentIndex > imageNumber) {
-                                                        recentIndex = 0;
-                                                    }
-                                                    articleImage = article.images.map((image, number)=> {
-                                                        if(number === recentIndex) {
-                                                            return (
-                                                                <img className="Article__image" key={number} style={showStyle} src={`http://localhost:5500/static/article/${image}`} alt={`${image}`}/>
-                                                            )
-                                                        }
-                                                        else if(number === prevNum) {
-                                                            return (
-                                                                <img className="Article__image" key={number} style={noShowStyle} src={`http://localhost:5500/static/article/${image}`} alt={`${image}`}/>
-                                                            )
-                                                        }
-                                                        else {
-                                                            return (
-                                                                <img className="Article__image" key={number} style={noShowStyle} src={`http://localhost:5500/static/article/${image}`} alt={`${image}`}/>
-                                                            )
-                                                        }
-                                                    })
-                                                }}>next</button>
-                                                <h2>{article.description}</h2>
-                                                <Moment format="LLLL" local>{article.date}</Moment>
+                                                    deleteArticle({ variables: { articleId: article._id } })
+                                                }}>
+                                                    Delete
+                                                </button>
                                             </div>
-                                            <div className="User__container">
-                                                <img className="User__profile" src={`http://localhost:5500/static/images/${article.writer.profile_image}`} alt={`${article.writer.name}'s image`}/>
-                                                <p className="User__name">{article.writer.name}</p>
-                                            </div>
-                                            <button className="Article__button" onClick={e => {
-                                                e.preventDefault();
-                                                deleteArticle({ variables: { articleId: article._id } })
-                                            }}>
-                                                Delete
-                                            </button>
-                                        </div>
-                                    )}                                    
-                                </Mutation>
-                            )
-                        })}
-                    </div>                        
-                );
-            }}
-        </Query>
-    )
+                                        )}                                    
+                                    </Mutation>
+                                )
+                            })}
+                        </div>                        
+                    );
+                }}
+            </Query>
+        )
+    }
 }
 
 export default Articles
