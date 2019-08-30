b9c99908111222140a7ca5b03999adc6b7bc9083
Janggeun Song <sjg1468@gmail.com>
2019-08-30 23:05:24 +0900

    Add article revice page

---
+++ client/pages/ArticleRevice.tsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+export class ArticleRevice extends Component {
+    componentDidMount() {
+        document.title = 'ArticleRevice';
+    }
+
+    render() {
+        return (
+            <div>
+                Hello This is Article Revice part page.
+            </div>
+        )
+    }
+}
+
+export default ArticleRevice
